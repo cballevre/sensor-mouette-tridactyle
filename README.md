de6f9e6c5ae20730f1b7d3ee4605d19927fed093
cballevre <celestin.ballevre@protonmail.com>
2025-06-25 11:11:18 +0200

    feat: Add humidity and temperature sensor

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 link to "Grove - Temperature&Humidity Sensor Pro(DHT22)" doc :
 [Link Text](https://wiki.seeedstudio.com/Grove-Temperature_and_Humidity_Sensor_Pro/)
+
+## Schema
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+## Libraries needed
+
+- https://github.com/Seeed-Studio/Grove_Temperature_And_Humidity_Sensor
--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,31 @@
+#include "Grove_Temperature_And_Humidity_Sensor.h"
+
+// Configuration for temperature and humidity
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define DHTPIN 25
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
   Serial.begin(9600);
+  Wire.begin();
+  dht.begin();
 }
 
 void loop() {
+  // Wind
   int wind = read_wind();
-  Serial.print(wind);
+  Serial.print("Wind speed : ");
+  Serial.println(wind);
+
+  // Temperature and humidity
+  float* temp_hum_val = read_temp_and_humidity();
+  Serial.print("Humidity : ");
+  Serial.print(temp_hum_val[0]);
+  Serial.print("%\t");
+  Serial.print("Temperature: ");
+  Serial.print(temp_hum_val[1]);
+  Serial.println("°C");
+
   delay(500);
 }
 
@@ -16,4 +37,20 @@ int read_wind() {
   float outvoltage = sensorValue * (3.3 / 4095.0);
   int level = 6 * outvoltage; // The level of wind speed is proportional to the output voltage.
   return level;
+}
+
+/**
+  Reading temperature or humidity takes about 250 milliseconds!
+  Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+*/
+float* read_temp_and_humidity() {
+  static float temp_hum_val[2] = {0};
+  if (!dht.readTempAndHumidity(temp_hum_val)) {
+    return temp_hum_val;
+  } else {
+    Serial.println("Failed to get temperature and humidity value.");
+    temp_hum_val[0] = 0;
+    temp_hum_val[1] = 0;
+    return temp_hum_val;
+  }
 }
